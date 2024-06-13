62a09efc0c8e29d5e48a8dfb1bcc5a0743f0ae5a
キクチケンタ <K023G0039@g.neec.ac.jp>
2024-06-13 12:23:10 +0900

    MT3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <Novice.h>
+#include <imgui.h>
+#include "Math/MathFunction.h"
+
+static const int kWindowWidth = 1280;
+static const int kWindowHeight = 720;
 
 const char kWindowTitle[] = "提出用課題";
 
 // Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) 
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 
 	// ライブラリの初期化
@@ -12,6 +17,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+
+	MathFunction Func;
+
+	int prevMouseX = 0;
+	int prevMouseY = 0;
+	bool isDragging = false;
+
+	Vector3 rotate = {};
+	Vector3 translate = {};
+	Vector3 cameraTranslate = { 0.0f, 1.9f, -6.49f };
+	Vector3 cameraRotate = { 0.26f, 0.0f, 0.0f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0)
@@ -23,9 +39,59 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
+		// マウス入力を取得
+		POINT mousePosition;
+		GetCursorPos(&mousePosition);
+
 		///
 		/// ↓更新処理ここから
 		///
+
+		  // マウスドラッグによる回転制御
+		if (Novice::IsPressMouse(1))
+		{
+			if (!isDragging)
+			{
+				isDragging = true;
+				prevMouseX = mousePosition.x;
+				prevMouseY = mousePosition.y;
+			}
+			else
+			{
+				int deltaX = mousePosition.x - prevMouseX;
+				int deltaY = mousePosition.y - prevMouseY;
+				rotate.y += deltaX * 0.01f; // 水平方向の回転
+				rotate.x += deltaY * 0.01f; // 垂直方向の回転
+				prevMouseX = mousePosition.x;
+				prevMouseY = mousePosition.y;
+			}
+		}
+		else
+		{
+			isDragging = false;
+		}
+
+		// マウスホイールで前後移動
+		int wheel = Novice::GetWheel();
+		if (wheel != 0)
+		{
+			cameraTranslate.z += wheel * 0.01f; // ホイールの回転方向に応じて前後移動
+		}
+
+		//各種行列の計算
+		Matrix4x4 worldMatrix = Func.MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
+		Matrix4x4 cameraMatrix = Func.MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		Matrix4x4 viewWorldMatrix = Func.Inverse(worldMatrix);
+		Matrix4x4 viewCameraMatrix = Func.Inverse(cameraMatrix);
+
+		// 透視投影行列を作成
+		Matrix4x4 projectionMatrix = Func.MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+
+		//ビュー座標変換行列を作成
+		Matrix4x4 viewProjectionMatrix = Func.Multiply(viewCameraMatrix, projectionMatrix);
+
+		//ViewportMatrixビューポート変換行列を作成
+		Matrix4x4 viewportMatrix = Func.MakeViewportMatrix(0.0f, 0.0f, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
 
 		///
 		/// ↑更新処理ここまで
@@ -35,6 +101,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		/// ↓描画処理ここから
 		///
 
+		// Gridを描画
+		Func.DrawGrid(viewProjectionMatrix, viewportMatrix);
+
 		///
 		/// ↑描画処理ここまで
 		///
@@ -43,7 +112,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		Novice::EndFrame();
 
 		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) 
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0)
 		{
 			break;
 		}
